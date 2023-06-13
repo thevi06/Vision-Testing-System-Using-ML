c8701b43cbe90c3cefc7ef07dfc228fc4c6382a5
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2023-06-13 22:03:30 +0530

    Update MainWindow.py

--- MainWindow.py
+++ MainWindow.py
@@ -85,3 +85,41 @@ def get_initial_details_of_center_of_the_eye(face_points, iris_points, eye_point
                                                       face_points[eye_points[2]]) / height + 1e-6
 
     return mean_reference_point, center_to_left_corner_horizontal_width, center_to_up_vertical_height
+
+def current_position_details(face_points):
+    # "minEnclosingCircle" opencv for finding the smallest circle
+    (x, y), radius = cv.minEnclosingCircle(face_points)
+    return [int(x), int(y), int(radius)]
+
+
+def filter_current_position(current_point, reference_point):
+    position_vector = np.zeros(9)
+    rho, theta = polar_coordinates(current_point, reference_point)
+    if rho < 0.2:
+        position_vector[0] = 1
+        # print("center")
+    if rho > 0.2 and 160 < theta < 200:
+        position_vector[1] = 1
+        # print("left")
+    if rho > 0.2 and 0 <= theta <= 20 or 340 <= theta <= 360:
+        position_vector[2] = 1
+        # print("right")
+    if rho > 0.2 and 70 <= theta <= 110:
+        position_vector[3] = 1
+        # print("up")
+    if rho > 0.2 and 250 <= theta <= 290:
+        position_vector[4] = 1
+        # print("down")
+    if rho > 0.2 and 20 < theta < 70:
+        position_vector[5] = 1
+        # print("right upper")
+    if rho > 0.2 and 110 < theta < 160:
+        position_vector[6] = 1
+        # print("left upper")
+    if rho > 0.2 and 200 < theta < 250:
+        position_vector[7] = 1
+        # print("left lower")
+    if rho > 0.2 and 290 < theta < 340:
+        position_vector[8] = 1
+        # print("right lower")
+    return position_vector
