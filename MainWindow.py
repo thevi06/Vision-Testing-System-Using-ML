e87d45bb717e18574d7bb27317b9cbe0527b5204
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2023-06-13 22:00:43 +0530

    Update MainWindow.py

--- MainWindow.py
+++ MainWindow.py
@@ -11,3 +11,19 @@ import math
 from time import time
 from Signin_Login_Page import Login
 import os
+
+with open("parameters.json", "r") as read_file:
+    param = json.load(read_file)
+
+
+def file_path_create():
+    desktop_path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')  # Get the desktop path
+    folder_name = "result_1"  # Set the folder name
+    #folder_path = os.path.join(desktop_path, folder_name)  # Create the full path to the new folder
+    folder_path = "result"
+    print(folder_path)
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)  # Create the new folder
+    else:
+        print("Folder already exists.")
+    return folder_path
