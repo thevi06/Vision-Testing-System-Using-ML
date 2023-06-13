5c22614c6f9a22a5686956256fc3f1c3b0242d4b
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2023-06-13 22:02:29 +0530

    Update MainWindow.py

--- MainWindow.py
+++ MainWindow.py
@@ -42,3 +42,24 @@ def iris_details(points):
 # normalize distance of points
 def normalized_distance(distance, total_distance):
     return round((distance / total_distance), 2)
+
+# convert rectangular to polar coordinates
+def polar_coordinates(point1, point2):
+    x1 = point1[0] - point2[0]
+    y1 = point1[1] - point2[1]
+    rho = math.sqrt(math.pow(x1, 2) + math.pow(y1, 2)) / (point2[2] + 1e-6)
+    theta = abs(math.degrees(math.atan(y1 / (x1 + 1e-6))))
+
+    if x1 > 0 and y1 > 0:
+        theta = 360 - theta
+
+    if x1 > 0 and y1 < 0:
+        theta = theta
+
+    if x1 < 0 and y1 < 0:
+        theta = 180 - theta
+
+    if x1 < 0 and y1 > 0:
+        theta = 180 + theta
+
+    return rho, theta
