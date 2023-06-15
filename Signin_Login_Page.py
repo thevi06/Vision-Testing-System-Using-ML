0045910ceb9005f544c933ab7d06d1f261d6128b
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2023-06-15 23:17:38 +0530

    Create Signin_Login_Page.py

---
+++ Signin_Login_Page.py
@@ -0,0 +1,8 @@
+import sys
+import mysql.connector
+from mysql.connector import errorcode
+from PyQt5.QtCore import QDate, pyqtSignal
+from PyQt5.QtGui import QIcon, QPixmap, QIntValidator
+from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QPushButton, QStackedWidget, QLabel, \
+    QLineEdit, QSizePolicy, QGridLayout, QDateEdit, QComboBox, QMessageBox, QMainWindow
+from InfantDetails_Form import MyPage
