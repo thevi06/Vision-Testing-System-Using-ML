de67717867859cd5ec49c69aacb3e06619bb9ea5
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2023-06-16 22:01:29 +0530

    Update Signin_Login_Page.py

--- Signin_Login_Page.py
+++ Signin_Login_Page.py
@@ -6,3 +6,7 @@ from PyQt5.QtGui import QIcon, QPixmap, QIntValidator
 from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QPushButton, QStackedWidget, QLabel, \
     QLineEdit, QSizePolicy, QGridLayout, QDateEdit, QComboBox, QMessageBox, QMainWindow
 from InfantDetails_Form import MyPage
+
+class Login(QWidget):  # Login Page
+    login_signal =pyqtSignal()
+    sendcursor = pyqtSignal(object)
