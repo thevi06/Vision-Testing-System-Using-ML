2ab1371720968ee59692fe94cfc504274a1e6036
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2023-06-16 22:02:18 +0530

    Update Signin_Login_Page.py

--- Signin_Login_Page.py
+++ Signin_Login_Page.py
@@ -10,3 +10,27 @@ from InfantDetails_Form import MyPage
 class Login(QWidget):  # Login Page
     login_signal =pyqtSignal()
     sendcursor = pyqtSignal(object)
+
+    def __init__(self):
+        super().__init__()
+
+
+        #self.signUp = Signup()
+        self.my_page = MyPage()
+
+        self.connect_to_db_done = True
+        self.input_details_correct = False
+        self.msg = QMessageBox
+
+        self.connect_to_db()
+        self.signUp = Signup()
+        self.signUp.sendSignal.connect(self.getData)
+
+        self.setWindowTitle('Login')
+        self.setWindowIcon(QIcon(''))
+        self.window_width, self.window_height = 600, 350
+        self.setFixedSize(self.window_width, self.window_height)
+
+        layout = QGridLayout()  # Layout
+        fulllayout = QHBoxLayout()
+        self.setLayout(fulllayout)
