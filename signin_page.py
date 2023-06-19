5ae125bfc5ff4db28db012b6064b1f63761bf17c
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2023-06-19 22:13:28 +0530

    Update signin_page.py

--- signin_page.py
+++ signin_page.py
@@ -7,3 +7,35 @@ from PyQt6.QtSql import QSqlQuery, QSqlDatabase
 from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QPushButton, QStackedWidget, QLabel, \
     QLineEdit, QSizePolicy, QGridLayout, QDateEdit, QComboBox, QMessageBox, QMainWindow
 from InfantDetails_Form import MyPage
+
+class Login(QWidget):  # Login Page
+    login_signal =pyqtSignal()
+    def __init__(self):
+        super().__init__()
+        self.signUp = Signup()
+        self.my_page = MyPage()
+        self.signUp.send_data.connect(self.send_data)
+        self.connect_to_db_done = True
+        self.input_details_correct = False
+        self.msg = QMessageBox
+
+        self.setWindowTitle('Login')
+        self.setWindowIcon(QIcon(''))
+        self.window_width, self.window_height = 600, 350
+        self.setFixedSize(self.window_width, self.window_height)
+
+        layout = QGridLayout()  # Layout
+        fulllayout = QHBoxLayout()
+        self.setLayout(fulllayout)
+
+        imglabel = QLabel(self)  # image
+        pixmap = QPixmap('baby.jpg')
+        imglabel.setPixmap(pixmap)
+        fulllayout.addWidget(imglabel)
+        fulllayout.addLayout(layout)
+
+        imglabel1 = QLabel(self)  # Logo
+        pixmap = QPixmap('logoMINI.JPG')
+        imglabel1.setPixmap(pixmap)
+        imglabel1.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+        layout.addWidget(imglabel1, 0, 2)
