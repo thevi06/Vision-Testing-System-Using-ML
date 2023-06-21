268ce1230147d37eb0b9a77729bb2db88ce145e4
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2023-06-21 22:46:12 +0530

    Update signin_page.py

--- signin_page.py
+++ signin_page.py
@@ -237,3 +237,14 @@ class Login(QWidget):  # Login Page
         # else:
         #     self.status.setText('enter details')
          #     self.lineEdits['details'].setText("")
+
+    def send_signal(self):
+        self.login_signal.emit()
+
+    def open_second_gui(self):
+        if self.connect_to_db_done:#######Signup page open function#######
+            self.signUp.show()
+            self.hide()
+            self.signUp.signal.connect(self.show)
+        else:
+            QMessageBox.critical(self, "Error", "No Data Base, Contact the IT team")
