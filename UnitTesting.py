89ff4f394c1fef63b68603c65790819010d99807
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2023-06-22 23:12:33 +0530

    Update UnitTesting.py

--- UnitTesting.py
+++ UnitTesting.py
@@ -2,3 +2,22 @@ import unittest
 from MainWindow import euclidean_distance
 import numpy as np
 import cv2
+
+class TestIrisDetails(unittest.TestCase):
+
+    def test_iris_details(self):
+        # create mock input data
+        points = [(0, 0), (0, 1), (1, 1), (1, 0)]
+        points = np.array(points)
+
+        # calculate expected output
+        expected_center = (0.5, 0.5)
+        expected_radius = 0.7071067811865476
+
+        # call the function with mock input data
+        (center, radius) = cv2.minEnclosingCircle(points)
+
+        # check if the actual output matches the expected output
+        self.assertAlmostEqual(center[0], expected_center[0], places=2)
+        self.assertAlmostEqual(center[1], expected_center[1], places=2)
+        self.assertAlmostEqual(radius, expected_radius, places=2)
