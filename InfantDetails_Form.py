3446b2b9b3536aa6246e04ceb1cda3fef5f57f66
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2023-06-26 22:32:25 +0530

    Update InfantDetails_Form.py

--- InfantDetails_Form.py
+++ InfantDetails_Form.py
@@ -63,3 +63,22 @@ class MyPage(QWidget):
 
         # Add the heading layout to the container layout
         container_layout.addLayout(heading_layout)
+
+        # Create the icon widget and add it to the container layout
+        icon = QLabel(self)
+        pixmap = QPixmap('ECTALogo.png').scaledToHeight(90)  # Set the height of the pixmap to 50 pixels
+        pixmap = pixmap.scaled(pixmap.width() // 2, pixmap.height() // 2)
+        icon.setPixmap(pixmap)
+        icon.setAlignment(Qt.AlignmentFlag.AlignTop)  # Align the icon to the top
+
+        # Create a vertical spacer item to push the form elements to the top of the container
+        vspacer = QWidget(self)
+        vspacer.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+
+        # Create a horizontal layout to add the vertical spacer, icon and spacer to the container layout
+        hbox = QHBoxLayout()
+        hbox.addWidget(heading_label)
+        hbox.addWidget(vspacer)
+        hbox.addWidget(icon)
+        hbox.setContentsMargins(0, 0, 0, 0)
+        hbox.setSpacing(0)
