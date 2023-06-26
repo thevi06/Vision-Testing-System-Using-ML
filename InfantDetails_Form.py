8ae425c7ef0e30824ef9d819fcb9baa7a6f6f3d2
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2023-06-26 22:30:59 +0530

    Update InfantDetails_Form.py

--- InfantDetails_Form.py
+++ InfantDetails_Form.py
@@ -12,3 +12,21 @@ class MyPage(QWidget):
 
     start_signal = pyqtSignal()
     close_signal = pyqtSignal(bool)
+
+    def __init__(self):
+        super().__init__()
+        self.connect_to_db = False
+        # Create the database and table
+        self.create_database_and_table()
+        self.database = MyWidget()
+
+        # Set the window size as a percentage of the screen size
+        screen_size = QApplication.primaryScreen().availableGeometry()
+        width_percent = 0.5
+        height_percent = 0.6
+        self.setGeometry(
+            int((1 - width_percent) * screen_size.width() / 2),
+            int((1 - height_percent) * screen_size.height() / 2),
+            int(width_percent * screen_size.width()),
+            int(height_percent * screen_size.height())
+        )
