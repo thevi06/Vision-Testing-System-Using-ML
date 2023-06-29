cf373bce8594c8b641a07192f92de15211f8f8d6
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2023-06-29 21:14:05 +0530

    Update Infants_Database.py

---
+++ Infants_Database.py
@@ -0,0 +1,3 @@
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QTableWidget, QTableWidgetItem, QPushButton, QLabel
+from PyQt6.QtGui import QPainter, QColor
+import mysql.connector
