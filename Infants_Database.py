511c9f7b481cc42c113e638d3ea24575a22728e3
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2023-06-29 21:14:39 +0530

    Update Infants_Database.py

--- Infants_Database.py
+++ Infants_Database.py
@@ -1,3 +1,10 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QTableWidget, QTableWidgetItem, QPushButton, QLabel
 from PyQt6.QtGui import QPainter, QColor
 import mysql.connector
+
+class MyWidget(QWidget):
+    def __init__(self):
+        super(MyWidget, self).__init__()
+
+        # Create main layout
+        self.layout = QVBoxLayout()
